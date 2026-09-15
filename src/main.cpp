38f098f319fde4d745cf4d9f62797668b5941a7e
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-16 02:55:38 +0500

    Add AssetManager to load xmas.xpk assets

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "AssetManager.h"
 
 int main() {
     std::cout << "Santa iOS Engine Started!" << std::endl;
+
+    AssetManager assetMgr;
+    // Hum assets folder se xmas.xpk load karne ki koshish karenge
+    if (assetMgr.loadXPK("assets/xmas.xpk")) {
+        std::cout << "XPK loaded successfully!" << std::endl;
+    } else {
+        std::cout << "Failed to load XPK. (Is the file in assets folder?)" << std::endl;
+    }
+
     return 0;
 }
