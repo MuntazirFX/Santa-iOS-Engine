a090ac44803371ebb91731b977b25425993173d1
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-17 00:35:29 +0500

    Remove loadDDSTextureNamed method from MetalView

--- ios/MetalView.h
+++ ios/MetalView.h
@@ -6,6 +6,5 @@
 @interface MetalView : MTKView <MTKViewDelegate>
 - (instancetype)initWithFrame:(CGRect)frame;
 - (void)setMeshToRender:(MeshData *)mesh;
-- (id<MTLTexture>)loadDDSTextureNamed:(NSString *)name debugOut:(NSMutableString *)dbg;
 @property (strong, nonatomic) NSString *textureDebugInfo;
 @end
