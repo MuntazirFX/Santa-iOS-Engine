87e6f5ffb69de27e93b0d4ea08da3ac8f1806d74
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-17 00:30:51 +0500

    Refactor AppDelegate to use shorter variable names

--- ios/AppDelegate.mm
+++ ios/AppDelegate.mm
@@ -6,44 +6,23 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
-    UIViewController *viewController = [[UIViewController alloc] init];
-    viewController.view.backgroundColor = [UIColor blackColor];
+    UIViewController *vc = [[UIViewController alloc] init];
+    vc.view.backgroundColor = [UIColor blackColor];
     
-    UITextView *textView = [[UITextView alloc] initWithFrame:viewController.view.bounds];
-    textView.backgroundColor = [UIColor blackColor];
-    textView.textColor = [UIColor greenColor];
-    textView.font = [UIFont fontWithName:@"Courier" size:10];
-    textView.editable = NO;
-    [viewController.view addSubview:textView];
+    UITextView *tv = [[UITextView alloc] initWithFrame:vc.view.bounds];
+    tv.backgroundColor = [UIColor blackColor];
+    tv.textColor = [UIColor greenColor];
+    tv.font = [UIFont fontWithName:@"Courier" size:10];
+    tv.editable = NO;
+    [vc.view addSubview:tv];
     
     NSMutableString *log = [NSMutableString string];
-    [log appendString:@"=== Mesh scan of all X-Files ===\n\n"];
+    [log appendString:@"=== Finding BIG meshes ===\n\n"];
+    [log appendString:[GameEngine findAllMeshes]];
     
-    // Saare offsets jo XPK scan se mile
-    NSArray *offsets = @[
-        @21047, @23006, @24801, @30231, @32146,
-        @33915, @35939, @38063, @41046, @42940,
-        @44576, @46942, @49451, @51810, @113582,
-        @116079, @118804, @120937, @123085, @126362,
-        @128111, @130435, @131615, @135241, @137486
-    ];
+    tv.text = log;
     
-    for (NSNumber *off in offsets) {
-        @autoreleasepool {
-            MeshData *mesh = [GameEngine extractMeshAtOffset:[off unsignedIntegerValue]];
-            if (mesh && mesh.vertexCount > 0) {
-                NSString *tex = mesh.textureName ? [mesh.textureName lastPathComponent] : @"(none)";
-                [log appendFormat:@"@%@: %d v, %d f, %@\n",
-                 off, mesh.vertexCount, mesh.faceCount, tex];
-            } else {
-                [log appendFormat:@"@%@: FAILED\n", off];
-            }
-        }
-    }
-    
-    textView.text = log;
-    
-    self.window.rootViewController = viewController;
+    self.window.rootViewController = vc;
     [self.window makeKeyAndVisible];
     
     return YES;
