adc295cd0cc4ca79ace3f0fe4b12534677e96d24
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-17 00:36:28 +0500

    Update font size and log message in AppDelegate

--- ios/AppDelegate.mm
+++ ios/AppDelegate.mm
@@ -12,19 +12,18 @@
     UITextView *tv = [[UITextView alloc] initWithFrame:vc.view.bounds];
     tv.backgroundColor = [UIColor blackColor];
     tv.textColor = [UIColor greenColor];
-    tv.font = [UIFont fontWithName:@"Courier" size:10];
+    tv.font = [UIFont fontWithName:@"Courier" size:9];
     tv.editable = NO;
     [vc.view addSubview:tv];
     
     NSMutableString *log = [NSMutableString string];
-    [log appendString:@"=== Finding BIG meshes ===\n\n"];
+    [log appendString:@"=== Finding Santa (big meshes) ===\n\n"];
     [log appendString:[GameEngine findAllMeshes]];
     
     tv.text = log;
     
     self.window.rootViewController = vc;
     [self.window makeKeyAndVisible];
-    
     return YES;
 }
 
