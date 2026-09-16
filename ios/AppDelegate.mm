dea8b588246a934ed7353fd8d1259285d8b283a8
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-16 14:26:31 +0500

    Fix screen bounds usage in AppDelegate

--- ios/AppDelegate.mm
+++ ios/AppDelegate.mm
@@ -8,16 +8,20 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
     NSString *engineStatus = [GameEngine startEngine];
-    NSLog(@"%@", engineStatus);
+    NSLog(@"[AppDelegate] %@", engineStatus);
     
     UIViewController *viewController = [[UIViewController alloc] init];
     viewController.view.backgroundColor = [UIColor blackColor];
     
-    MetalView *metalView = [[MetalView alloc] initWithFrame:viewController.view.bounds];
+    // AHEM FIX: UIScreen.mainScreen.bounds use karein, na ke viewController.view.bounds
+    CGRect screenBounds = [[UIScreen mainScreen] bounds];
+    NSLog(@"[AppDelegate] Screen bounds: %.0fx%.0f", screenBounds.size.width, screenBounds.size.height);
+    
+    MetalView *metalView = [[MetalView alloc] initWithFrame:screenBounds];
     metalView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     [viewController.view addSubview:metalView];
     
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 50, viewController.view.bounds.size.width, 100)];
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 50, screenBounds.size.width, 100)];
     label.text = engineStatus;
     label.numberOfLines = 0;
     label.textColor = [UIColor whiteColor];
