377367f9a52faa34c0683a949ec2f7d0270c3551
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-16 22:21:04 +0500

    Replace UITextView with MetalView for rendering

--- ios/AppDelegate.mm
+++ ios/AppDelegate.mm
@@ -1,5 +1,6 @@
 #import "AppDelegate.h"
 #import "GameEngine.h"
+#import "MetalView.h"
 
 @implementation AppDelegate
 
@@ -9,35 +10,31 @@
     UIViewController *viewController = [[UIViewController alloc] init];
     viewController.view.backgroundColor = [UIColor blackColor];
     
-    UITextView *textView = [[UITextView alloc] initWithFrame:viewController.view.bounds];
-    textView.backgroundColor = [UIColor blackColor];
-    textView.textColor = [UIColor greenColor];
-    textView.font = [UIFont fontWithName:@"Courier" size:9];
-    textView.editable = NO;
-    [viewController.view addSubview:textView];
+    MetalView *metalView = [[MetalView alloc] initWithFrame:viewController.view.bounds];
+    metalView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [viewController.view addSubview:metalView];
+    
+    UITextView *logView = [[UITextView alloc] initWithFrame:CGRectMake(0, 40, viewController.view.bounds.size.width, 90)];
+    logView.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.6];
+    logView.textColor = [UIColor greenColor];
+    logView.font = [UIFont fontWithName:@"Courier" size:10];
+    logView.editable = NO;
+    logView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    [viewController.view addSubview:logView];
     
     NSMutableString *log = [NSMutableString string];
     
-    // DDS files with 'haus'
-    [log appendString:@"=== 'haus' files ===\n"];
-    [log appendString:[GameEngine listAssetsByKeyword:@"haus"]];
-    [log appendString:@"\n"];
+    MeshData *mesh = [GameEngine extractFirstMeshAtOffset:21047];
+    if (mesh && mesh.vertexCount > 0 && mesh.faceCount > 0) {
+        [log appendFormat:@"Mesh: %d verts, %d faces\n", mesh.vertexCount, mesh.faceCount];
+        [log appendFormat:@"UVs: %@\n", mesh.uvs ? @"yes" : @"no"];
+        [log appendFormat:@"Tex: haus2.dds (from XPK)\n"];
+        [metalView setMeshToRender:mesh];
+    } else {
+        [log appendString:@"Mesh extraction failed\n"];
+    }
     
-    // DDS files with 'dach'
-    [log appendString:@"=== 'dach' files ===\n"];
-    [log appendString:[GameEngine listAssetsByKeyword:@"dach"]];
-    [log appendString:@"\n"];
-    
-    // All DDS files
-    [log appendString:@"=== All .dds (first 40) ===\n"];
-    [log appendString:[GameEngine listAssetsByKeyword:@".dds"]];
-    [log appendString:@"\n"];
-    
-    // All TGA files
-    [log appendString:@"=== All .tga (first 20) ===\n"];
-    [log appendString:[GameEngine listAssetsByKeyword:@".tga"]];
-    
-    textView.text = log;
+    logView.text = log;
     
     self.window.rootViewController = viewController;
     [self.window makeKeyAndVisible];
