aab07496b2bb17c62cd4a6094e467d031b5df650
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-16 17:21:31 +0500

    Add XFileParser header with XToken structure

---
+++ src/XFileParser.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <cstdint>
+#include <string>
+#include <vector>
+
+struct XToken {
+    int type;
+    std::string name;
+    int intValue;
+    std::vector<int> intList;
+    std::vector<float> floatList;
+    float floatValue;
+    int dwordValue;
+    int wordValue;
+};
+
+class XFileParser {
+public:
+    static std::vector<XToken> parseTokens(const uint8_t* data, size_t size, int maxTokens);
+    static std::string describeToken(const XToken& token);
+};
