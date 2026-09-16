cf8c69a4f85769a81e1164a4c15563b812b17ffd
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-17 02:01:44 +0500

    Integrate MetalView and update UI components
    Replaced UITextView with MetalView for rendering, updated text view properties, and added mesh extraction for Santa.

--- ios/AppDelegate.mm
+++ ios/AppDelegate.mm
@@ -1,5 +1,6 @@
 #import "AppDelegate.h"
 #import "GameEngine.h"
+#import "MetalView.h"
 
 @implementation AppDelegate
 
@@ -9,16 +10,30 @@
     UIViewController *vc = [[UIViewController alloc] init];
     vc.view.backgroundColor = [UIColor blackColor];
     
-    UITextView *tv = [[UITextView alloc] initWithFrame:vc.view.bounds];
-    tv.backgroundColor = [UIColor blackColor];
+    MetalView *mv = [[MetalView alloc] initWithFrame:vc.view.bounds];
+    mv.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [vc.view addSubview:mv];
+    
+    UITextView *tv = [[UITextView alloc] initWithFrame:CGRectMake(0, 40, vc.view.bounds.size.width, 100)];
+    tv.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.7];
     tv.textColor = [UIColor greenColor];
-    tv.font = [UIFont fontWithName:@"Courier" size:9];
+    tv.font = [UIFont fontWithName:@"Courier" size:10];
     tv.editable = NO;
+    tv.autoresizingMask = UIViewAutoresizingFlexibleWidth;
     [vc.view addSubview:tv];
     
     NSMutableString *log = [NSMutableString string];
-    [log appendString:@"=== Searching for Santa ===\n\n"];
-    [log appendString:[GameEngine findSantaModel]];
+    
+    MeshData *mesh = [GameEngine extractSantaMesh];
+    
+    if (mesh && mesh.vertexCount > 0 && mesh.faceCount > 0) {
+        [log appendFormat:@"🎅 Santa!\n"];
+        [log appendFormat:@"%d verts, %d faces\n", mesh.vertexCount, mesh.faceCount];
+        [log appendFormat:@"Tex: %@\n", mesh.textureName ? [mesh.textureName lastPathComponent] : @"(none)"];
+        [mv setMeshToRender:mesh];
+    } else {
+        [log appendString:@"Santa extraction failed"];
+    }
     
     tv.text = log;
     
