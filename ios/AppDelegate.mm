8986db75d64f889a877549971b85d3131c869992
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-16 05:08:22 +0500

    Modify AppDelegate to change label text and start engine
    Updated label text and added GameEngine initialization.

--- ios/AppDelegate.mm
+++ ios/AppDelegate.mm
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import "GameEngine.h"
 
 @implementation AppDelegate
 
@@ -6,16 +7,21 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
     UIViewController *viewController = [[UIViewController alloc] init];
-    viewController.view.backgroundColor = [UIColor colorWithRed:0.1 green:0.1 blue:0.2 alpha:1.0]; // Dark blue background
+    viewController.view.backgroundColor = [UIColor colorWithRed:0.1 green:0.1 blue:0.2 alpha:1.0];
     
     UILabel *label = [[UILabel alloc] initWithFrame:viewController.view.bounds];
-    label.text = @"Santa iOS Engine Started!";
+    label.text = @"Santa iOS Engine\nLoading XPK...";
+    label.numberOfLines = 0;
     label.textColor = [UIColor whiteColor];
     label.textAlignment = NSTextAlignmentCenter;
+    label.font = [UIFont boldSystemFontOfSize:24];
     [viewController.view addSubview:label];
     
     self.window.rootViewController = viewController;
     [self.window makeKeyAndVisible];
+    
+    // Engine start karein
+    [GameEngine startEngine];
     
     return YES;
 }
