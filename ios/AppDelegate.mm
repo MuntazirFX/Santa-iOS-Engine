258e193614337128ce2910c87abbfa78098f4ad3
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-17 02:33:29 +0500

    Replace loading label with a text view for output

--- ios/AppDelegate.mm
+++ ios/AppDelegate.mm
@@ -1,6 +1,5 @@
 #import "AppDelegate.h"
 #import "GameEngine.h"
-#import "MetalView.h"
 
 @implementation AppDelegate
 
@@ -10,36 +9,22 @@
     UIViewController *vc = [[UIViewController alloc] init];
     vc.view.backgroundColor = [UIColor blackColor];
     
-    // Loading label (Santa scan takes 10-20 sec)
-    UILabel *loading = [[UILabel alloc] initWithFrame:vc.view.bounds];
-    loading.text = @"Scanning for Santa...\n(This takes ~15 seconds)";
-    loading.numberOfLines = 0;
-    loading.textAlignment = NSTextAlignmentCenter;
-    loading.textColor = [UIColor greenColor];
-    loading.backgroundColor = [UIColor blackColor];
-    loading.font = [UIFont fontWithName:@"Courier" size:14];
-    [vc.view addSubview:loading];
+    UITextView *tv = [[UITextView alloc] initWithFrame:vc.view.bounds];
+    tv.backgroundColor = [UIColor blackColor];
+    tv.textColor = [UIColor greenColor];
+    tv.font = [UIFont fontWithName:@"Courier" size:9];
+    tv.editable = NO;
+    [vc.view addSubview:tv];
     
-    // Add Metal view behind
-    MetalView *mv = [[MetalView alloc] initWithFrame:vc.view.bounds];
-    mv.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    [vc.view insertSubview:mv atIndex:0];
+    tv.text = @"Scanning for Santa (30 sec)...";
     
     self.window.rootViewController = vc;
     [self.window makeKeyAndVisible];
     
-    // Do the scan on a background thread so UI doesn't freeze
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-        MeshData *mesh = [GameEngine findSantaMesh];
-        
+        NSString *result = [GameEngine scanForSantaModel];
         dispatch_async(dispatch_get_main_queue(), ^{
-            if (mesh && mesh.vertexCount > 0 && mesh.faceCount > 0) {
-                [loading removeFromSuperview];
-                [mv setMeshToRender:mesh];
-                NSLog(@"✅ Santa loaded: %d v, %d f", mesh.vertexCount, mesh.faceCount);
-            } else {
-                loading.text = @"Santa not found!\nCheck console for logs.";
-            }
+            tv.text = result;
         });
     });
     
