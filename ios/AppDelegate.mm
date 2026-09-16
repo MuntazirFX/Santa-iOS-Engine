5b3f6971f746ebca4d3e6ea28bbfea41c5d45920
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-16 19:40:03 +0500

    Adjust font size and enhance mesh extraction output
    Updated font size in UITextView and modified mesh extraction logic to display vertex information.

--- ios/AppDelegate.mm
+++ ios/AppDelegate.mm
@@ -12,13 +12,27 @@
     UITextView *textView = [[UITextView alloc] initWithFrame:viewController.view.bounds];
     textView.backgroundColor = [UIColor blackColor];
     textView.textColor = [UIColor greenColor];
-    textView.font = [UIFont fontWithName:@"Courier" size:8];
+    textView.font = [UIFont fontWithName:@"Courier" size:10];
     textView.editable = NO;
     [viewController.view addSubview:textView];
     
-    // 300 tokens tak scan karein
-    NSString *result = [GameEngine parseXFileAtOffset:21047 maxTokens:300];
-    textView.text = result;
+    MeshData *mesh = [GameEngine extractFirstMeshAtOffset:21047];
+    
+    NSMutableString *out = [NSMutableString string];
+    if (mesh) {
+        [out appendFormat:@"Mesh extracted!\n"];
+        [out appendFormat:@"Vertex count: %d\n\n", mesh.vertexCount];
+        
+        const float *v = (const float *)mesh.vertices.bytes;
+        int show = mesh.vertexCount > 10 ? 10 : mesh.vertexCount;
+        for (int i = 0; i < show; i++) {
+            [out appendFormat:@"v%d: (%.2f, %.2f, %.2f)\n", i, v[i*3], v[i*3+1], v[i*3+2]];
+        }
+    } else {
+        [out appendString:@"Mesh extraction failed!\n"];
+    }
+    
+    textView.text = out;
     
     self.window.rootViewController = viewController;
     [self.window makeKeyAndVisible];
