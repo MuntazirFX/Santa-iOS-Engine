dbfdc1e992a9d9d6bc76c56aa768c2c35924d232
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-17 00:26:48 +0500

    Add mesh scanning and logging functionality

--- ios/AppDelegate.mm
+++ ios/AppDelegate.mm
@@ -17,12 +17,29 @@
     [viewController.view addSubview:textView];
     
     NSMutableString *log = [NSMutableString string];
+    [log appendString:@"=== Mesh scan of all X-Files ===\n\n"];
     
-    [log appendString:@"=== X-Files in XPK ===\n\n"];
-    [log appendString:[GameEngine scanXPKForXFiles]];
-    [log appendString:@"\n"];
-    [log appendString:@"=== weihnachts files ===\n"];
-    [log appendString:[GameEngine listAssetsByKeyword:@"weihnachts"]];
+    // Saare offsets jo XPK scan se mile
+    NSArray *offsets = @[
+        @21047, @23006, @24801, @30231, @32146,
+        @33915, @35939, @38063, @41046, @42940,
+        @44576, @46942, @49451, @51810, @113582,
+        @116079, @118804, @120937, @123085, @126362,
+        @128111, @130435, @131615, @135241, @137486
+    ];
+    
+    for (NSNumber *off in offsets) {
+        @autoreleasepool {
+            MeshData *mesh = [GameEngine extractMeshAtOffset:[off unsignedIntegerValue]];
+            if (mesh && mesh.vertexCount > 0) {
+                NSString *tex = mesh.textureName ? [mesh.textureName lastPathComponent] : @"(none)";
+                [log appendFormat:@"@%@: %d v, %d f, %@\n",
+                 off, mesh.vertexCount, mesh.faceCount, tex];
+            } else {
+                [log appendFormat:@"@%@: FAILED\n", off];
+            }
+        }
+    }
     
     textView.text = log;
     
