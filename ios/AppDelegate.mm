c185167f863e5a9b7ec1ec0e0e39278697c966d2
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-16 22:59:50 +0500

    Replace MetalView with UITextView for logging
    Replaced MetalView with UITextView for displaying logs and removed debug overlay.

--- ios/AppDelegate.mm
+++ ios/AppDelegate.mm
@@ -1,6 +1,5 @@
 #import "AppDelegate.h"
 #import "GameEngine.h"
-#import "MetalView.h"
 
 @implementation AppDelegate
 
@@ -10,31 +9,20 @@
     UIViewController *viewController = [[UIViewController alloc] init];
     viewController.view.backgroundColor = [UIColor blackColor];
     
-    MetalView *metalView = [[MetalView alloc] initWithFrame:viewController.view.bounds];
-    metalView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    [viewController.view addSubview:metalView];
-    
-    // Debug output overlay
-    UITextView *logView = [[UITextView alloc] initWithFrame:CGRectMake(0, 40, viewController.view.bounds.size.width, 280)];
-    logView.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.75];
-    logView.textColor = [UIColor greenColor];
-    logView.font = [UIFont fontWithName:@"Courier" size:9];
-    logView.editable = NO;
-    logView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-    [viewController.view addSubview:logView];
+    UITextView *textView = [[UITextView alloc] initWithFrame:viewController.view.bounds];
+    textView.backgroundColor = [UIColor blackColor];
+    textView.textColor = [UIColor greenColor];
+    textView.font = [UIFont fontWithName:@"Courier" size:10];
+    textView.editable = NO;
+    [viewController.view addSubview:textView];
     
     NSMutableString *log = [NSMutableString string];
     
-    MeshData *mesh = [GameEngine extractFirstMeshAtOffset:21047];
-    if (mesh && mesh.vertexCount > 0 && mesh.faceCount > 0) {
-        [log appendFormat:@"Mesh: %d verts, %d faces\n\n", mesh.vertexCount, mesh.faceCount];
-        [metalView setMeshToRender:mesh];
-        [log appendString:metalView.textureDebugInfo];
-    } else {
-        [log appendString:@"Mesh extraction failed\n"];
-    }
+    // XPK mein DDS files scan karein
+    [log appendString:@"=== Scanning XPK for 'DDS ' signature ===\n\n"];
+    [log appendString:[GameEngine scanXPKForDDS]];
     
-    logView.text = log;
+    textView.text = log;
     
     self.window.rootViewController = viewController;
     [self.window makeKeyAndVisible];
