6b504dc4ec0735c1115134667f887c36d348f872
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-16 19:59:50 +0500

    Add method to set mesh for rendering in MetalView

--- ios/MetalView.h
+++ ios/MetalView.h
@@ -1,6 +1,9 @@
 #import <UIKit/UIKit.h>
 #import <MetalKit/MetalKit.h>
 
+@class MeshData;
+
 @interface MetalView : MTKView <MTKViewDelegate>
 - (instancetype)initWithFrame:(CGRect)frame;
+- (void)setMeshToRender:(MeshData *)mesh;
 @end
