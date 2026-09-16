76991367d09e593ddb8ed81ef227836df3cd19fc
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-16 14:23:46 +0500

    Initialize Metal pipeline and command queue
    Updated MetalView to initialize pipeline state and command queue. Added vertex and fragment function loading from the default library.

--- ios/MetalView.mm
+++ ios/MetalView.mm
@@ -1,19 +1,85 @@
 #import "MetalView.h"
 #import <Metal/Metal.h>
-#import <QuartzCore/CAMetalLayer.h>
+#import <simd/simd.h>
 
-@implementation MetalView
+@implementation MetalView {
+    id<MTLDevice> _device;
+    id<MTLCommandQueue> _commandQueue;
+    id<MTLRenderPipelineState> _pipelineState;
+}
 
 - (instancetype)initWithFrame:(CGRect)frame {
-    id<MTLDevice> device = MTLCreateSystemDefaultDevice();
-    self = [super initWithFrame:frame device:device];
+    _device = MTLCreateSystemDefaultDevice();
+    self = [super initWithFrame:frame device:_device];
     if (self) {
         self.clearColor = MTLClearColorMake(0.1, 0.1, 0.2, 1.0);
         self.colorPixelFormat = MTLPixelFormatBGRA8Unorm;
         self.preferredFramesPerSecond = 60;
-        NSLog(@"[MetalView] Metal Device: %@", device.name);
+        self.delegate = self;
+        
+        _commandQueue = [_device newCommandQueue];
+        
+        // Shader library load karein (default.metallib)
+        NSError *error = nil;
+        id<MTLLibrary> library = [_device newDefaultLibrary];
+        if (!library) {
+            NSLog(@"[MetalView] ERROR: Could not load default library!");
+            return self;
+        }
+        
+        // Vertex aur fragment functions dhoondein
+        id<MTLFunction> vertexFunction = [library newFunctionWithName:@"vertex_main"];
+        id<MTLFunction> fragmentFunction = [library newFunctionWithName:@"fragment_main"];
+        
+        if (!vertexFunction || !fragmentFunction) {
+            NSLog(@"[MetalView] ERROR: Could not find shader functions!");
+            return self;
+        }
+        
+        // Render pipeline descriptor banayein
+        MTLRenderPipelineDescriptor *pipelineDescriptor = [[MTLRenderPipelineDescriptor alloc] init];
+        pipelineDescriptor.vertexFunction = vertexFunction;
+        pipelineDescriptor.fragmentFunction = fragmentFunction;
+        pipelineDescriptor.colorAttachments[0].pixelFormat = self.colorPixelFormat;
+        
+        _pipelineState = [_device newRenderPipelineStateWithDescriptor:pipelineDescriptor error:&error];
+        if (!_pipelineState) {
+            NSLog(@"[MetalView] ERROR: Pipeline creation failed: %@", error);
+        } else {
+            NSLog(@"[MetalView] Pipeline created successfully!");
+        }
     }
     return self;
+}
+
+- (void)mtkView:(MTKView *)view drawableSizeWillChange:(CGSize)size {
+}
+
+- (void)drawInMTKView:(MTKView *)view {
+    if (!_pipelineState) return;
+    
+    // Triangle ke 3 vertices (X, Y, Z, W, R, G, B, A)
+    // Screen coordinates: -1 to 1
+    static const float vertices[] = {
+        // Position (X, Y, Z, W)     Color (R, G, B, A)
+         0.0,  0.8, 0.0, 1.0,       1.0, 0.0, 0.0, 1.0,  // Top (Red)
+        -0.8, -0.8, 0.0, 1.0,       0.0, 1.0, 0.0, 1.0,  // Bottom-left (Green)
+         0.8, -0.8, 0.0, 1.0,       0.0, 0.0, 1.0, 1.0,  // Bottom-right (Blue)
+    };
+    
+    MTLRenderPassDescriptor *renderPassDescriptor = view.currentRenderPassDescriptor;
+    if (renderPassDescriptor == nil) return;
+    
+    id<MTLCommandBuffer> commandBuffer = [_commandQueue commandBuffer];
+    id<MTLRenderCommandEncoder> encoder = [commandBuffer renderCommandEncoderWithDescriptor:renderPassDescriptor];
+    
+    [encoder setRenderPipelineState:_pipelineState];
+    [encoder setVertexBytes:vertices length:sizeof(vertices) atIndex:0];
+    [encoder drawPrimitives:MTLPrimitiveTypeTriangle vertexStart:0 vertexCount:3];
+    [encoder endEncoding];
+    
+    [commandBuffer presentDrawable:view.currentDrawable];
+    [commandBuffer commit];
 }
 
 @end
