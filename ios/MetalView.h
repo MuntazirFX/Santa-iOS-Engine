d6528967ef862d27583cbcbee4e4addd7e093d99
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-16 14:14:20 +0500

    Refactor MetalView class implementation
    Refactor MetalView to inherit from MTKView and update imports.

--- ios/MetalView.h
+++ ios/MetalView.h
@@ -1,19 +1,6 @@
-#import "MetalView.h"
-#import <Metal/Metal.h>
-#import <QuartzCore/CAMetalLayer.h>
+#import <UIKit/UIKit.h>
+#import <MetalKit/MetalKit.h>
 
-@implementation MetalView
-
-- (instancetype)initWithFrame:(CGRect)frame {
-    id<MTLDevice> device = MTLCreateSystemDefaultDevice();
-    self = [super initWithFrame:frame device:device];
-    if (self) {
-        self.clearColor = MTLClearColorMake(0.1, 0.1, 0.2, 1.0);
-        self.colorPixelFormat = MTLPixelFormatBGRA8Unorm;
-        self.preferredFramesPerSecond = 60;
-        NSLog(@"[MetalView] Metal Device: %@", device.name);
-    }
-    return self;
-}
-
+@interface MetalView : MTKView
+- (instancetype)initWithFrame:(CGRect)frame;
 @end
