14d4850a14fc20585c41f366499a7614fd6a5a5c
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-16 19:38:06 +0500

    Add MeshData interface and update GameEngine

--- ios/GameEngine.h
+++ ios/GameEngine.h
@@ -1,7 +1,15 @@
 #import <Foundation/Foundation.h>
 
+@interface MeshData : NSObject
+@property (nonatomic) int vertexCount;
+@property (nonatomic) int faceCount;
+@property (strong, nonatomic) NSMutableData *vertices;  // float3 array
+@property (strong, nonatomic) NSMutableData *uvs;       // float2 array
+@end
+
 @interface GameEngine : NSObject
 + (NSString *)startEngine;
 + (NSData *)loadAssetNamed:(NSString *)name;
 + (NSString *)parseXFileAtOffset:(NSUInteger)offset maxTokens:(int)maxTokens;
++ (MeshData *)extractFirstMeshAtOffset:(NSUInteger)offset;
 @end
