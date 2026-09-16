93fd11f07860c562a06d6c6efae9b0805f4772a5
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-16 14:15:49 +0500

    Clean up comments in AppDelegate.mm
    Removed comments in AppDelegate.mm for clarity.

--- ios/AppDelegate.mm
+++ ios/AppDelegate.mm
@@ -7,20 +7,16 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
-    // Engine start karein
     NSString *engineStatus = [GameEngine startEngine];
     NSLog(@"%@", engineStatus);
     
-    // Base view controller banayein
     UIViewController *viewController = [[UIViewController alloc] init];
     viewController.view.backgroundColor = [UIColor blackColor];
     
-    // Metal view ko addSubview ke zariye add karein
     MetalView *metalView = [[MetalView alloc] initWithFrame:viewController.view.bounds];
     metalView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     [viewController.view addSubview:metalView];
     
-    // Status label upar add karein
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 50, viewController.view.bounds.size.width, 100)];
     label.text = engineStatus;
     label.numberOfLines = 0;
