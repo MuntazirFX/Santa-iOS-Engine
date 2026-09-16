5ecbda687a8979aef10e3d5aa1cf060f49333efc
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-16 21:51:14 +0500

    Refactor MetalView and logView initialization

--- ios/AppDelegate.mm
+++ ios/AppDelegate.mm
@@ -10,19 +10,18 @@
     UIViewController *viewController = [[UIViewController alloc] init];
     viewController.view.backgroundColor = [UIColor blackColor];
     
-    // Status label overlay
-    UITextView *logView = [[UITextView alloc] initWithFrame:CGRectMake(0, 40, viewController.view.bounds.size.width, 140)];
-    logView.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.7];
+    MetalView *metalView = [[MetalView alloc] initWithFrame:viewController.view.bounds];
+    metalView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [viewController.view addSubview:metalView];
+    
+    // Status label (small, top)
+    UITextView *logView = [[UITextView alloc] initWithFrame:CGRectMake(0, 40, viewController.view.bounds.size.width, 90)];
+    logView.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.6];
     logView.textColor = [UIColor greenColor];
     logView.font = [UIFont fontWithName:@"Courier" size:10];
     logView.editable = NO;
     logView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
     [viewController.view addSubview:logView];
-    
-    // Metal view below
-    MetalView *metalView = [[MetalView alloc] initWithFrame:viewController.view.bounds];
-    metalView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    [viewController.view insertSubview:metalView atIndex:0];
     
     NSMutableString *log = [NSMutableString string];
     
@@ -30,14 +29,13 @@
     if (mesh && mesh.vertexCount > 0 && mesh.faceCount > 0) {
         [log appendFormat:@"Mesh: %d verts, %d faces\n", mesh.vertexCount, mesh.faceCount];
         [log appendFormat:@"UVs: %@\n", mesh.uvs ? @"yes" : @"no"];
-        [log appendFormat:@"Texture: %@\n", mesh.textureName ?: @"(none)"];
+        [log appendFormat:@"Tex: %@\n", mesh.textureName ? [mesh.textureName lastPathComponent] : @"(none)"];
         [metalView setMeshToRender:mesh];
     } else {
         [log appendString:@"Mesh extraction failed\n"];
     }
     
     logView.text = log;
-    NSLog(@"[AppDelegate] %@", log);
     
     self.window.rootViewController = viewController;
     [self.window makeKeyAndVisible];
