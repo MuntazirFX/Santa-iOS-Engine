d3d4558f4343496588c756c2247a961307f81ad5
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-16 20:01:05 +0500

    Integrate MetalView for mesh rendering
    Replaced UITextView with MetalView for rendering mesh data.

--- ios/AppDelegate.mm
+++ ios/AppDelegate.mm
@@ -1,5 +1,6 @@
 #import "AppDelegate.h"
 #import "GameEngine.h"
+#import "MetalView.h"
 
 @implementation AppDelegate
 
@@ -9,32 +10,16 @@
     UIViewController *viewController = [[UIViewController alloc] init];
     viewController.view.backgroundColor = [UIColor blackColor];
     
-    UITextView *textView = [[UITextView alloc] initWithFrame:viewController.view.bounds];
-    textView.backgroundColor = [UIColor blackColor];
-    textView.textColor = [UIColor greenColor];
-    textView.font = [UIFont fontWithName:@"Courier" size:11];
-    textView.editable = NO;
-    [viewController.view addSubview:textView];
+    MetalView *metalView = [[MetalView alloc] initWithFrame:viewController.view.bounds];
+    metalView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [viewController.view addSubview:metalView];
     
+    // Extract mesh and send to Metal view
     MeshData *mesh = [GameEngine extractFirstMeshAtOffset:21047];
-    
-    NSMutableString *out = [NSMutableString string];
     if (mesh) {
-        [out appendFormat:@"✓ Mesh extracted!\n"];
-        [out appendFormat:@"Vertices: %d\n", mesh.vertexCount];
-        [out appendFormat:@"Faces: %d\n\n", mesh.faceCount];
-        
-        const float *v = (const float *)mesh.vertices.bytes;
-        int show = mesh.vertexCount > 15 ? 15 : mesh.vertexCount;
-        [out appendString:@"First vertices:\n"];
-        for (int i = 0; i < show; i++) {
-            [out appendFormat:@"v%d: (%.3f, %.3f, %.3f)\n", i, v[i*3], v[i*3+1], v[i*3+2]];
-        }
-    } else {
-        [out appendString:@"Mesh extraction failed!\nCheck logs for details."];
+        [metalView setMeshToRender:mesh];
+        NSLog(@"[AppDelegate] Mesh sent: %d verts, %d faces", mesh.vertexCount, mesh.faceCount);
     }
-    
-    textView.text = out;
     
     self.window.rootViewController = viewController;
     [self.window makeKeyAndVisible];
