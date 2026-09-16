d54ba890ee49dfba055f86285327814b210799e1
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-17 02:11:05 +0500

    Add loading label and update Metal view handling
    Added a loading label to inform users about the scanning process for Santa. Modified the Metal view insertion and removed the previous UITextView logging mechanism.

--- ios/AppDelegate.mm
+++ ios/AppDelegate.mm
@@ -10,35 +10,39 @@
     UIViewController *vc = [[UIViewController alloc] init];
     vc.view.backgroundColor = [UIColor blackColor];
     
+    // Loading label (Santa scan takes 10-20 sec)
+    UILabel *loading = [[UILabel alloc] initWithFrame:vc.view.bounds];
+    loading.text = @"Scanning for Santa...\n(This takes ~15 seconds)";
+    loading.numberOfLines = 0;
+    loading.textAlignment = NSTextAlignmentCenter;
+    loading.textColor = [UIColor greenColor];
+    loading.backgroundColor = [UIColor blackColor];
+    loading.font = [UIFont fontWithName:@"Courier" size:14];
+    [vc.view addSubview:loading];
+    
+    // Add Metal view behind
     MetalView *mv = [[MetalView alloc] initWithFrame:vc.view.bounds];
     mv.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    [vc.view addSubview:mv];
-    
-    UITextView *tv = [[UITextView alloc] initWithFrame:CGRectMake(0, 40, vc.view.bounds.size.width, 100)];
-    tv.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.7];
-    tv.textColor = [UIColor greenColor];
-    tv.font = [UIFont fontWithName:@"Courier" size:10];
-    tv.editable = NO;
-    tv.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-    [vc.view addSubview:tv];
-    
-    NSMutableString *log = [NSMutableString string];
-    
-    MeshData *mesh = [GameEngine extractSantaMesh];
-    
-    if (mesh && mesh.vertexCount > 0 && mesh.faceCount > 0) {
-        [log appendFormat:@"🎅 Santa!\n"];
-        [log appendFormat:@"%d verts, %d faces\n", mesh.vertexCount, mesh.faceCount];
-        [log appendFormat:@"Tex: %@\n", mesh.textureName ? [mesh.textureName lastPathComponent] : @"(none)"];
-        [mv setMeshToRender:mesh];
-    } else {
-        [log appendString:@"Santa extraction failed"];
-    }
-    
-    tv.text = log;
+    [vc.view insertSubview:mv atIndex:0];
     
     self.window.rootViewController = vc;
     [self.window makeKeyAndVisible];
+    
+    // Do the scan on a background thread so UI doesn't freeze
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+        MeshData *mesh = [GameEngine findSantaMesh];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (mesh && mesh.vertexCount > 0 && mesh.faceCount > 0) {
+                [loading removeFromSuperview];
+                [mv setMeshToRender:mesh];
+                NSLog(@"✅ Santa loaded: %d v, %d f", mesh.vertexCount, mesh.faceCount);
+            } else {
+                loading.text = @"Santa not found!\nCheck console for logs.";
+            }
+        });
+    });
+    
     return YES;
 }
 
