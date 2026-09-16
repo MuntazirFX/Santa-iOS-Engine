892a7fa9445cf947ab3b54dbc3febfd9892dd519
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-17 01:27:57 +0500

    Refactor AppDelegate to remove MetalView and adjust UITextView
    Removed MetalView initialization and related mesh extraction code. Updated UITextView frame and background color.

--- ios/AppDelegate.mm
+++ ios/AppDelegate.mm
@@ -1,6 +1,5 @@
 #import "AppDelegate.h"
 #import "GameEngine.h"
-#import "MetalView.h"
 
 @implementation AppDelegate
 
@@ -10,32 +9,16 @@
     UIViewController *vc = [[UIViewController alloc] init];
     vc.view.backgroundColor = [UIColor blackColor];
     
-    MetalView *mv = [[MetalView alloc] initWithFrame:vc.view.bounds];
-    mv.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    [vc.view addSubview:mv];
-    
-    UITextView *tv = [[UITextView alloc] initWithFrame:CGRectMake(0, 40, vc.view.bounds.size.width, 100)];
-    tv.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.7];
+    UITextView *tv = [[UITextView alloc] initWithFrame:vc.view.bounds];
+    tv.backgroundColor = [UIColor blackColor];
     tv.textColor = [UIColor greenColor];
-    tv.font = [UIFont fontWithName:@"Courier" size:10];
+    tv.font = [UIFont fontWithName:@"Courier" size:9];
     tv.editable = NO;
-    tv.autoresizingMask = UIViewAutoresizingFlexibleWidth;
     [vc.view addSubview:tv];
     
     NSMutableString *log = [NSMutableString string];
-    
-    // Try @171669 (704 verts, 869 faces) — likely a big model
-    MeshData *mesh = [GameEngine extractMeshAtOffset:960968];
-    
-    if (mesh && mesh.vertexCount > 0 && mesh.faceCount > 0) {
-        [log appendFormat:@"Model @960968\n"];
-        [log appendFormat:@"%d verts, %d faces\n", mesh.vertexCount, mesh.faceCount];
-        [log appendFormat:@"Tex: %@\n\n", mesh.textureName ?: @"(none)"];
-        [mv setMeshToRender:mesh];
-        [log appendString:mv.textureDebugInfo];
-    } else {
-        [log appendString:@"Extraction failed!\n"];
-    }
+    [log appendString:@"=== Searching for Santa ===\n\n"];
+    [log appendString:[GameEngine findSantaModel]];
     
     tv.text = log;
     
