db9204dd5a3775bfc361c5f951ec0a21e5c932d6
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-16 21:26:25 +0500

    Add status label overlay and update mesh logging

--- ios/AppDelegate.mm
+++ ios/AppDelegate.mm
@@ -10,15 +10,34 @@
     UIViewController *viewController = [[UIViewController alloc] init];
     viewController.view.backgroundColor = [UIColor blackColor];
     
+    // Status label overlay
+    UITextView *logView = [[UITextView alloc] initWithFrame:CGRectMake(0, 40, viewController.view.bounds.size.width, 140)];
+    logView.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.7];
+    logView.textColor = [UIColor greenColor];
+    logView.font = [UIFont fontWithName:@"Courier" size:10];
+    logView.editable = NO;
+    logView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    [viewController.view addSubview:logView];
+    
+    // Metal view below
     MetalView *metalView = [[MetalView alloc] initWithFrame:viewController.view.bounds];
     metalView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    [viewController.view addSubview:metalView];
+    [viewController.view insertSubview:metalView atIndex:0];
+    
+    NSMutableString *log = [NSMutableString string];
     
     MeshData *mesh = [GameEngine extractFirstMeshAtOffset:21047];
-    if (mesh) {
+    if (mesh && mesh.vertexCount > 0 && mesh.faceCount > 0) {
+        [log appendFormat:@"Mesh: %d verts, %d faces\n", mesh.vertexCount, mesh.faceCount];
+        [log appendFormat:@"UVs: %@\n", mesh.uvs ? @"yes" : @"no"];
+        [log appendFormat:@"Texture: %@\n", mesh.textureName ?: @"(none)"];
         [metalView setMeshToRender:mesh];
-        NSLog(@"[AppDelegate] Mesh sent to view");
+    } else {
+        [log appendString:@"Mesh extraction failed\n"];
     }
+    
+    logView.text = log;
+    NSLog(@"[AppDelegate] %@", log);
     
     self.window.rootViewController = viewController;
     [self.window makeKeyAndVisible];
