07840e6e4b4eed6e1cc32a45c05504d4a176176c
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-17 01:29:49 +0500

    Add method to find all meshes in GameEngine

--- ios/GameEngine.h
+++ ios/GameEngine.h
@@ -14,5 +14,6 @@
 + (NSData *)loadAssetNamed:(NSString *)name;
 + (MeshData *)extractMeshAtOffset:(NSUInteger)offset;
 + (NSString *)listAssetsByKeyword:(NSString *)keyword;
++ (NSString *)findAllMeshes;
 + (NSString *)findSantaModel;
 @end
