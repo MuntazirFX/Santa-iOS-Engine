1e47c45518e6f547d19dbf0516e829ac6267bb11
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-16 14:00:18 +0500

    Integrate MetalView and modify status label
    Added MetalView and updated label positioning and font size.

--- ios/AppDelegate.mm
+++ ios/AppDelegate.mm
@@ -1,25 +1,28 @@
 #import "AppDelegate.h"
 #import "GameEngine.h"
+#import "MetalView.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
-    UIViewController *viewController = [[UIViewController alloc] init];
-    viewController.view.backgroundColor = [UIColor colorWithRed:0.1 green:0.1 blue:0.2 alpha:1.0];
+    // Engine start karein
+    NSString *engineStatus = [GameEngine startEngine];
+    NSLog(@"%@", engineStatus);
     
-    UILabel *label = [[UILabel alloc] initWithFrame:viewController.view.bounds];
+    // Metal view banayein aur screen par dikhayein
+    MetalView *metalView = [[MetalView alloc] initWithFrame:self.window.bounds];
+    UIViewController *viewController = [[UIViewController alloc] init];
+    viewController.view = metalView;
+    
+    // Status label upar add karein
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 50, self.window.bounds.size.width, 100)];
+    label.text = engineStatus;
     label.numberOfLines = 0;
     label.textColor = [UIColor whiteColor];
     label.textAlignment = NSTextAlignmentCenter;
-    label.font = [UIFont systemFontOfSize:14];
-    
-    // Engine chala kar result screen par dikhayein
-    NSString *engineStatus = [GameEngine startEngine];
-    label.text = engineStatus;
-    NSLog(@"%@", engineStatus);
-    
+    label.font = [UIFont systemFontOfSize:12];
     [viewController.view addSubview:label];
     
     self.window.rootViewController = viewController;
