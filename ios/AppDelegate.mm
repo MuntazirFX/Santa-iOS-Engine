c234394f4d1ef9e88a41a410b8964e3bc5c45549
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-17 18:33:17 +0500

    Update UITextView size and text formatting

--- ios/AppDelegate.mm
+++ ios/AppDelegate.mm
@@ -14,23 +14,23 @@
     mv.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     [vc.view addSubview:mv];
     
-    UITextView *tv = [[UITextView alloc] initWithFrame:CGRectMake(0, 40, vc.view.bounds.size.width, 110)];
-    tv.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.75];
+    // LARGE text view to show bbox info
+    UITextView *tv = [[UITextView alloc] initWithFrame:CGRectMake(0, 40, vc.view.bounds.size.width, 250)];
+    tv.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.85];
     tv.textColor = [UIColor greenColor];
-    tv.font = [UIFont fontWithName:@"Courier" size:10];
+    tv.font = [UIFont fontWithName:@"Courier" size:9];
     tv.editable = NO;
     tv.autoresizingMask = UIViewAutoresizingFlexibleWidth;
     [vc.view addSubview:tv];
     
     MeshData *mesh = [GameEngine extractSantaWithTransforms:960968];
     
-    if (mesh && mesh.vertexCount > 0 && mesh.faceCount > 0) {
-        NSString *tex = mesh.textureName ? [mesh.textureName lastPathComponent] : @"(none)";
-        tv.text = [NSString stringWithFormat:@"🎅 MESH COLOR TEST\n%d v, %d faces\nTex: %@\n\n🔴 Red = Santa | 🟢 Green = Tree\n🟡 Yellow = House | 🔵 Blue = Misc",
-                   mesh.vertexCount, mesh.faceCount, tex];
+    if (mesh && mesh.vertexCount > 0) {
+        tv.text = [NSString stringWithFormat:@"MESH BBOX REPORT\n%d v, %d f\n\n%@",
+                   mesh.vertexCount, mesh.faceCount, mesh.debugInfo ?: @"(no debug)"];
         [mv setMeshToRender:mesh];
     } else {
-        tv.text = @"Santa extraction failed!";
+        tv.text = @"Extraction failed";
     }
     
     self.window.rootViewController = vc;
