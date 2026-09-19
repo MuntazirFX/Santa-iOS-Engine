22f925f6688452df005c9146345ab85293bca9ff
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-19 11:05:44 +0500

    Add method to list texture files in GameEngine

--- ios/GameEngine.h
+++ ios/GameEngine.h
@@ -22,9 +22,10 @@
 + (MeshData *)extractMeshFromAsset:(NSString *)assetName;
 + (NSArray<LevelObject *> *)parseLevelData:(NSString *)levelPath;
 + (NSString *)listLevelFiles;
++ (NSString *)listTextureFiles;
+
 // Decodes a DDS texture from the XPK archive (by its XPK-internal path,
 // e.g. "maps\\nicolaus.dds") into tightly-packed RGBA8 pixels.
-// Returns nil on failure; otherwise outWidth/outHeight are filled in.
 + (NSData *)loadTextureRGBA8Named:(NSString *)xpkPath
                              width:(int *)outWidth
                             height:(int *)outHeight;
